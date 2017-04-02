e381183ea630617b17652b1f7770fbcac1f30cfe
Americool <aberanderson@gmail.com>
2017-04-02 13:43:23 -0400

    base api controller & security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 end

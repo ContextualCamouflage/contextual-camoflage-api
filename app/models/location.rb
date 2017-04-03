5984550a4eb47d3d8702592c511fbcc5b90fb431
Ryan Milstead <ryanmilstead1@gmail.com>
2017-04-03 09:24:46 -0400

    Udpate location and story model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,3 @@
 class Location < ApplicationRecord
-  geocoded_by :address
-  reverse_geocoded_by :latitude, :longitude
-  belongs_to :story
-
-  def address
-    [street, city, state, country].compact.join(', ')
-  end
+  has_many :stories
 end

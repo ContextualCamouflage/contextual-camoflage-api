5bc22c108cbb3c022d8e404b9e1db976322d9e93
Ryan Milstead <ryanmilstead1@gmail.com>
2017-04-05 16:33:00 -0400

    Small upadtes

--- app/controllers/api/v1/anecdotes_controller.rb
+++ app/controllers/api/v1/anecdotes_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class AnecdoteController < ApiController
+    class AnecdotesController < ApiController
 
     def index
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :submissions, only: [:create, :index]
-      resources :anecdotes, only: [:create, :random]
+      resources :anecdotes, only: [:create]
+      get '/anecdotes/random', to: 'anecdotes#random'
       resources :researches, only: [:create]
     end
   end

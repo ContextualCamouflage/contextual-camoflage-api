b22ad76cc28ec454ca40d453010f3c161c77399b
Ryan Milstead <ryanmilstead1@gmail.com>
2017-04-06 08:40:48 -0400

    Updarte illness controller

--- app/controllers/api/v1/illnesses_controller.rb
+++ app/controllers/api/v1/illnesses_controller.rb
@@ -1,15 +1,17 @@
 module Api
   module V1
     class IllnessesController < ApiController
-    def show
-      illness = Illness.find_by_id(params[:id])
-      render json: illness.to_json, status: 200
-    end
 
-    private
-      def anecdote_params
-        params.require(:illness).permit(:id)
+      def index
+        @illnesses = Illness.all
+        render json: @illnesses.to_json, status 200
       end
+
+      def show
+        illness = Illness.find_by_id(params[:id])
+        render json: illness.to_json, status: 200
+      end
+
     end
   end
 end

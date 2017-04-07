7f66e2c17ef9f8188e5f393315c2ad7e2c8f3a66
Brittany Martin <brittany.jill.martin@gmail.com>
2017-04-07 09:44:41 -0400

    Remove Auth for debugging purposes

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ApiController < ApplicationController
-      before_action :authenticated?
+      #before_action :authenticated?
 
       private
       def authenticated?
